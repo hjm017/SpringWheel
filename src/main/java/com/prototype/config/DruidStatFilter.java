f1d81ea0414b6db98e88dd1aa4f205e18ca3561d
hejm <1003930282@qq.com>
2016-06-05 20:25:16 +0800

    添加druid监控后台

---
+++ src/main/java/com/prototype/config/DruidStatFilter.java
@@ -0,0 +1,19 @@
+package com.prototype.config;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * Druid的StatFilter
+ *
+ * @author hjm
+ * @create 2016年3月17日
+ */
+@WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*",
+        initParams = {
+                @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
+        })
+public class DruidStatFilter extends WebStatFilter {
+}
