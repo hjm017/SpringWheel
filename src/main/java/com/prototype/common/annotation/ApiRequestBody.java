edfe6b7d1bd5deda8ffe662142c26014cdff03be
hjm017 <1003930282@qq.com>
2016-06-24 17:50:16 +0800

    添加两个自定义注解

---
+++ src/main/java/com/prototype/common/annotation/ApiRequestBody.java
@@ -0,0 +1,16 @@
+package com.prototype.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * User: hejm
+ * Date: 2016/6/24
+ * Time: 17:47
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ApiRequestBody {
+
+    boolean required() default true;
+}
