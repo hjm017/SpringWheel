9239eacc65bf59bc86914e87ba7435a9a95867a7
hjm017 <1003930282@qq.com>
2016-06-24 17:42:46 +0800

    更新api方法拦截器

--- src/main/java/com/prototype/interceptor/ApiMethodInterceptor.java
+++ src/main/java/com/prototype/interceptor/ApiMethodInterceptor.java
@@ -8,7 +8,6 @@ import com.prototype.common.validation.ValidationResult;
 import com.prototype.common.validation.ValidationUtils;
 import org.apache.commons.io.IOUtils;
 import org.hibernate.validator.internal.engine.ValidatorImpl;
-import org.omg.IOP.Encoding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
