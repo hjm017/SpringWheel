293617c1e088e6f24a4852600ea9d1ffbf829412
hjm017 <1003930282@qq.com>
2016-06-27 16:10:34 +0800

    更改apiException显示

--- src/main/java/com/springwheel/api/UserEndpoint.java
+++ src/main/java/com/springwheel/api/UserEndpoint.java
@@ -1,13 +1,12 @@
 package com.springwheel.api;
 
+import com.springwheel.api.dto.UserDto;
 import com.springwheel.common.annotation.ApiController;
 import com.springwheel.common.annotation.ApiRequestBody;
 import com.springwheel.common.exception.ApiException;
-import com.springwheel.common.exception.ErrorCode;
 import com.springwheel.common.mapper.BeanMapper;
 import com.springwheel.common.util.constants.MediaTypes;
 import com.springwheel.data.domain.User;
-import com.springwheel.api.dto.UserDto;
 import com.springwheel.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +33,7 @@ public class UserEndpoint {
     public List<UserDto> getList() {
         List<User> users = userService.getList();
         if (false) {
-            throw new ApiException("这是一个测试例子", ErrorCode.INTERNAL_SERVER_ERROR);
+            throw new ApiException("这是一个测试例子");
         }
         return BeanMapper.mapList(users, UserDto.class);
     }
